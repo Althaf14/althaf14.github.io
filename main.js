9e1ead473bc1d2b49460994c213fac2d9c7ae2bb
Althaf14 <althaf1426@gmail.com>
2025-01-13 18:06:38 +0530

    responsive

--- main.js
+++ main.js
@@ -1,18 +1,29 @@
-const nav = document.querySelector(".nav");
+const nav = document.getElementById("nav");
 let lastScrollY = window.scrollY;
 
-window.addEventListener("scroll",()=>{
-    if(lastScrollY < window.scrollY){
-        nav.classList.add("nav-hidden");
-        
-    }
-    else if(lastScrollY > window.scrollY){
-        nav.classList.remove("nav-hidden");
-        nav.classList.add("new-nav");
-    }
-    else if( lastScrollY === window.scrollY){
-        nav.classList.remove("new-nav");
-    }
+window.addEventListener("scroll", () => {
+  if (lastScrollY < window.scrollY) {
+    nav.classList.add("nav-hidden");
+    nav.style.background = "none";
+  } else if (lastScrollY > window.scrollY) {
+    nav.classList.remove("nav-hidden");
+    nav.classList.add("new-nav");
+  }
+  
+  if (lastScrollY < 100  ) {
+    nav.classList.remove("new-nav");
+    nav.style.background = "none";
+  }
+  else if (lastScrollY == 653) {
+    nav.classList.remove("new-nav");
+    nav.style.background = "none";
+  } else {
+    nav.style.background = " #ffffff07";
+  }
 
-    lastScrollY = window.scrollY;
-})
+
+  
+
+  console.log(lastScrollY);
+  lastScrollY = window.scrollY;
+});
