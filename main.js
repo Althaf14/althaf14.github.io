0348a3d54b04037cc8a935cdcf0183b9715695e0
Althaf14 <althaf1426@gmail.com>
2025-01-28 21:14:22 +0530

    update

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ let lastScrollY = window.scrollY;
 let menu = document.getElementById("menu");
 let menuIcon = document.getElementById("menu-icon");
 let closeBtn = document.getElementById("closeBtn");
+let responsiveNav = document.getElementById("responsive-nav");
 
 window.addEventListener("scroll", () => {
   if (lastScrollY < window.scrollY) {
@@ -37,3 +38,5 @@ function menuCloseFn(){
   menu.style.display="none";
   menuIcon.style.display="block";
 }
+
+
